3f6432104f1f1ac7a300785bd13d5cce21279001
zetro-nayatel <zeeshan.azhar@nayatel.com>
2024-01-02 11:48:06 +0500

    git init

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
+import {useSnapshot} from 'valtio'
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
+
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
